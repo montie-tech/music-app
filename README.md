3f66c9f550a0e471062846f4945ae1e8bbd8e1ee
montie-tech <heliumcarmongmail.com>
2026-06-20 18:13:13 +0300

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-Music web app
-Stream music mixes and single songs in website
---
+++ convex/schema.ts
@@ -0,0 +1,21 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+
+export default defineSchema({
+
+  songs: defineTable({
+
+    title: v.string(),
+
+    artist: v.string(),
+
+    image: v.string(),
+
+    audioUrl: v.string(),
+
+    type: v.string(),
+
+  })
+
+});
